122f90bd61cd29945366b99b80f30db3b6c8f305
Thomas Sileghem <th.sileghem@gmail.com>
2017-01-04 14:54:51 +0000

    test(query): make sure query is return right ids

--- packages/nosqwal-test/test/query.js
+++ packages/nosqwal-test/test/query.js
@@ -86,3 +86,30 @@ test('query using wrong operator', async t => {
         }
     }));
 });
+
+test('make sure ids are valids', async t => {
+    const userCollection = getCollection();
+
+    await Promise.all([...Array(10)].map((_, i) => {
+        return userCollection.create({
+            name: `user-${i}`
+        });
+    }));
+
+    const users = await userCollection.query({
+        orderBy: [
+            ['name']
+        ]
+    });
+
+    t.is((await userCollection.get(users[0].id)).name, 'user-0');
+    t.is((await userCollection.get(users[1].id)).name, 'user-1');
+    t.is((await userCollection.get(users[2].id)).name, 'user-2');
+    t.is((await userCollection.get(users[3].id)).name, 'user-3');
+    t.is((await userCollection.get(users[4].id)).name, 'user-4');
+    t.is((await userCollection.get(users[5].id)).name, 'user-5');
+    t.is((await userCollection.get(users[6].id)).name, 'user-6');
+    t.is((await userCollection.get(users[7].id)).name, 'user-7');
+    t.is((await userCollection.get(users[8].id)).name, 'user-8');
+    t.is((await userCollection.get(users[9].id)).name, 'user-9');
+})
